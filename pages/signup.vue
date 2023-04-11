a0b94de88afb3f1ed2eff952499336cdf31e75ed
Akramsarker <dev.mdakram@gmail.com>
2023-04-11 08:04:08 +0600

    Implement sign up page

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,37 +1,48 @@
 <template>
-  <div class="signup-page">
-    <h1 class="heading-text">Sign up to get started</h1>
-    <div v-if="error" class="error-message">{{ error }}</div>
-    <form class="form-section" @submit.prevent="signUpWithEmail">
-      <div class="input-field">
-        <label for="email">Email</label>
-        <input v-model="email" type="email" placeholder="email" />
-      </div>
-      <div class="input-field">
-        <label for="password">Password</label>
-        <input v-model="password" type="password" placeholder="password" />
-      </div>
-      <button class="btn btn-primary" type="submit">Register</button>
-      <p class="or">Or</p>
-      <button class="btn btn-tertiary google" @click="signInWithGoogle()">
-        <!-- <img src="../assets/google.webp" />  -->
-        <span>Continue with Google</span>
-      </button>
-      <button class="btn btn-primary facebook" @click="signInWithFacebook()">
-        <!-- <img src="../assets/facebook.svg" /> -->
-        <span>Continue with Facebook</span>
-      </button>
-      <p>
-        Already have account?
-        <nuxt-link class="nuxt-link" to="/signin">Sign In</nuxt-link>
+  <div class="container">
+    <div class="sign-in-container">
+      <p v-if="error" class="error-message">
+        {{ error }}
       </p>
-    </form>
+      <div class="anima-section">
+        <div class="avatar-card">
+          <img src="~/static/avatar.png" alt="Avatar" />
+        </div>
+        <div class="sign-up-header">
+          <h1 class="header-name">Create Account</h1>
+          <p>
+            Create an account to save your progress and <br />
+            get access to conversations on other devices
+          </p>
+        </div>
+        <div class="sign-in-field">
+          <div class="input-field">
+            <input v-model="email" type="email" placeholder="Email" />
+          </div>
+          <div class="input-field">
+            <input v-model="password" type="password" placeholder="Password" />
+          </div>
+          <div class="input-field">
+            <input
+              v-model="repeatPassword"
+              type="password"
+              placeholder="Repeat Password"
+            />
+          </div>
+        </div>
+        <div class="sign-in-btn">
+          <button class="sign-in" @click="signUpWithEmail">
+            Create Account
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import JSConfetti from 'js-confetti'
 import firebase from 'firebase/app'
-
 export default {
   data() {
     return {
@@ -46,39 +57,56 @@ export default {
         .auth()
         .createUserWithEmailAndPassword(this.email, this.password)
         .then((data) => {
-          this.$router.push({ name: 'index' })
+          this.$router.push({ name: 'success' })
+          this.confetty()
         })
         .catch((error) => {
           this.error = error.message
         })
     },
-    signInWithGoogle() {
-      const provider = new firebase.auth.GoogleAuthProvider()
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then((data) => {
-          this.$router.push({ name: 'index' })
-        })
-        .catch(() => {
-          this.error = 'Sorry, something went wront. Please try again'
-        })
-    },
-    signInWithFacebook() {
-      const provider = new firebase.auth.FacebookAuthProvider()
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then((result) => {
-          const token = result.credential.accessToken
-          // eslint-disable-next-line no-console
-          console.log(result.user)
-          localStorage.setItem('facebookAccessToken', token)
-          this.$router.push({ name: 'index' })
-        })
-        .catch(() => {
-          this.error = 'Sorry, something went wront. Please try again'
-        })
+    confetty() {
+      const confetti = new JSConfetti()
+      confetti.addConfetti({
+        // emojis: [
+        //   '🌈',
+        //   '⚡️',
+        //   '💥',
+        //   '✨',
+        //   '💫',
+        //   '🌸',
+        //   '🌾',
+        //   '🌻',
+        //   '🌟',
+        //   '♥️',
+        //   '💐',
+        //   '🌷',
+        //   '🌹',
+        //   '🌱',
+        //   '🌿',
+        //   '☘️',
+        //   '🍀',
+        // ],
+        confettiColors: [
+          '#ff0a54',
+          '#f51966',
+          '#ff477e',
+          '#ff7e7e',
+          '#ff7096',
+          '#22c10d',
+          '#00ff83',
+          '#ff85a1',
+          '#0d32e8',
+          '#f6ff00',
+          '#fbb1bd',
+          '#f9bec7',
+          '#800080',
+          '#F0F8FF',
+          '#0000FF',
+          '#a546cb',
+        ],
+        confettiNumber: 500,
+        emojiSize: 100,
+      })
     },
   },
 }
@@ -86,72 +114,79 @@ export default {
 
 <style lang="scss" scoped>
 @import '~/styles/style.scss';
-.signup-page {
+.container {
+  position: relative;
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto auto auto;
+}
+
+.container::before {
+  background: #0a0a0d;
+  content: '';
+  position: absolute;
+  inset: 0px;
+  z-index: -100;
+  background-image: url('~/static/header-gradient.svg'),
+    url('~/static/footer-gradient.svg');
+  background-repeat: no-repeat;
+  background-position: center top, center bottom;
+  background-size: 1400px;
+}
+
+.sign-in-container {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  font-family: $font-primary;
-  @include responsive(phone) {
-    height: 100%;
-    padding: 0 1rem;
-  }
-  .heading-text {
-    margin-bottom: 2rem;
-    font-size: 2rem;
-    @include responsive(phone) {
-      font-size: 1.5rem;
-    }
-  }
+  margin: auto;
+  height: 100vh;
   .error-message {
-    margin-bottom: 1rem;
+    font-size: 1rem;
     color: red;
-    max-width: 500px;
-    line-height: 1.6;
+    padding-bottom: 1rem;
   }
-  .form-section {
-    display: flex;
-    flex-direction: column;
-    padding: 3rem 4rem;
-    border-radius: 0.5rem;
-    background: white;
-    box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.2);
-    @include responsive(phone) {
-      padding: 1rem 2rem;
-    }
-    .input-field {
+  .anima-section {
+    text-align: center;
+    .avatar-card {
       display: flex;
-      flex-direction: column;
-      margin-bottom: 1rem;
-      label {
-        font-size: 1rem;
-        margin-bottom: 0.5rem;
-      }
-    }
-    .or {
-      padding: 0.5rem 0;
-    }
-    .google {
-      display: flex;
+      justify-content: center;
       align-items: center;
-      color: black;
-      margin-bottom: 1rem;
-      &:hover {
-        color: white;
+      margin-bottom: 2rem;
+      img {
+        width: 120px;
+        height: 120px;
+        border-radius: 50%;
       }
     }
-    .facebook {
-      background-color: #3a559f;
-      margin-bottom: 1rem;
-      &:hover {
-        opacity: 0.9;
+    .sign-up-header {
+      .header-name {
+        line-height: 36px;
+        font-weight: 600;
+        font-size: 33px;
+        color: rgb(255, 255, 255);
+      }
+      p {
+        margin-top: 28px;
+        color: rgba(255, 255, 255, 0.5);
+        line-height: 19.2px;
+        font-size: 16px;
+        margin-bottom: 3rem;
       }
     }
-    p {
-      font-size: 0.9rem;
-      text-align: center;
-      .nuxt-link {
-        color: red;
+    .sign-in-btn {
+      .sign-in {
+        background-color: rgb(255, 255, 255);
+        border-radius: 50px;
+        padding: 0.7rem 2.2rem;
+        outline: none;
+        border: none;
+        font-size: 18px;
+        font-weight: 700;
+        line-height: 22px;
+        cursor: pointer;
+        opacity: 0.5;
+        color: rgb(0, 0, 0);
       }
     }
   }
